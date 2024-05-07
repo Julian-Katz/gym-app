b009ea1279267f51c337c2241774692377b6c5a0
Julian Katz <juliankatz@gmx.de>
2024-05-07 15:25:27 +0200

    change to md3

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -7,6 +7,7 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import { md3 } from 'vuetify/blueprints'
 
 // Composables
 import { createVuetify } from 'vuetify'
@@ -16,4 +17,5 @@ export default createVuetify({
   theme: {
     defaultTheme: 'dark',
   },
+  blueprint: md3,
 })
