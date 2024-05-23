0b5e9a9984625a30e9bf76d6c75780cc74254169
Julian Katz <juliankatz@gmx.de>
2024-05-23 12:05:14 +0200

    add navigation bar

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,11 +3,25 @@
     <v-main>
       <router-view />
     </v-main>
+    <v-bottom-navigation class="position-fixed" color=primary>
+      <v-btn to="/exercises">
+        <v-icon>mdi-dumbbell</v-icon>
+        <span>Exercises</span>
+      </v-btn>
 
-    <AppFooter />
+      <v-btn to="/workouts">
+        <v-icon>mdi-flag-checkered</v-icon>
+        <span>Workouts</span>
+      </v-btn>
+
+      <v-btn to="/profile">
+        <v-icon>mdi-account</v-icon>
+        <span>Profil</span>
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  //
+//
 </script>
